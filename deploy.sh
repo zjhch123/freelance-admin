9b64fd3d20eb721dd61b6d0c2c89c7d1054a8e46
zjhch123 <416836508@qq.com>
2018-08-23 16:28:06 +0800

    feat: 增加部署脚本与部署命令

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
-source /etc/profile
-cnpm install
-cnpm run build
-cd /var/node_server/freelancer-server/app/public/
-rm -rf admin
-mv /var/packages/freelancer-admin/dist /var/node_server/freelancer-server/app/public/admin
-cd /var/node_server/freelancer-server/
-npm run stop
-npm run start
+npm run build
+cd dist
+zip -r dist.zip ./*
+scp dist.zip root@47.104.237.102:/root/
+ssh root@47.104.237.102 '
+  cd /root && 
+  mv dist.zip /var/node_server/freelance-serve/app/otherPublic/admin/ && 
+  cd /var/node_server/freelance-serve/app/otherPublic/admin/ && 
+  unzip -o dist.zip && 
+  rm -rf dist.zip &&
+  source /etc/profile && 
+  npm run stop && 
+  npm run start
+'
